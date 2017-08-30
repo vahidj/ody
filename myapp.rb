fe09252b3c2cf3522588254b577ec26e1572fb2c
v0j0017 <vjalali@walmartlabs.com>
2017-08-30 16:49:34 -0700

    added more scenarios

--- myapp.rb
+++ myapp.rb
@@ -60,6 +60,7 @@ post '/events' do
 		if event.subtype && event.subtype == "file_share"
           case event.file.name
             when "desktop.jpeg"
+				text = "Here is what I found:"
 				attachments = [
                 {
                  "title": "Hisense Chromebook (11.6\" Quad-Core Processor)",
@@ -99,6 +100,7 @@ post '/events' do
         }
     ].to_json
             when "infant_milk.jpeg"
+			  text = "Here is what I found:"
               attachments = [        
                   {
                     "title": "Similac Pro-Advance Infant Formula",
@@ -143,75 +145,152 @@ post '/events' do
             when /^hi|hello|hey.*/
               urls = "Hi! Nice to meet you. I am Ody, the walmart.com shopping assistant! You can paste an url or upload an image here. I will try my best to find the most relevant products for you from walmart.com."
             when "<https://www.babycenter.com/best-convertible-car-seat>"
+			  text = "Here is what I found:"
 			  attachments = [
         {			
             "title": "Graco 4Ever All-in-1 Convertible Car Seat",
 			      "title_link": "https://www.walmart.com/ip/Graco-4Ever-All-in-1-Convertible-Car-Seat-Choose-Your-Pattern/46335604",
 			      "image_url":"https://i5.walmartimages.com/asr/b27da759-3f31-4727-a22f-3d7bc1c41a07_1.02cf3c75fc81a513eed09a662e59266e.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
         }, 
-        {"title":"Add To Cart", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D46335604%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"},
+        {"title":"ADD TO CART", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D46335604%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"},
         {			
             "title": "Graco Contender 65 Convertible Car Seat",
 			      "title_link": "https://www.walmart.com/ip/Graco-Contender-65-Convertible-Car-Seat-Choose-Your-Pattern/33396182",
 			      "image_url":"https://i5.walmartimages.com/asr/328b980a-9762-422a-8f8d-53bf171b3ff5_1.b6eff89073debf58491355b572ef5936.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
         }, 
-        {"title":"Add To Cart", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D33396182%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"},
+        {"title":"ADD TO CART", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D33396182%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"},
         {			
             "title": "Diono Radian RXT Convertible Car Seat",
 			      "title_link": "https://www.walmart.com/ip/Diono-Radian-RXT-Convertible-Car-Seat-Black-Cobalt/50878012",
 			      "image_url":"https://i5.walmartimages.com/asr/0303efbc-d205-4c51-bd39-139af9e831f6_1.939d16a6711f79943f3b21bd347d45d3.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
         }, 
-        {"title":"Add To Cart", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D50878012%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"},
+        {"title":"ADD TO CART", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D50878012%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"},
         {			
             "title": "Britax Marathon ClickTight Convertible Car Seat",
 			      "title_link": "https://www.walmart.com/ip/Britax-Marathon-ClickTight-Convertible-Car-Seat-Vue/55425979",
 			      "image_url":"https://i5.walmartimages.com/asr/61a8921a-b7a9-4401-8492-30a460bae83d_1.b8efebb059f3922398599d3919743129.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
         }, 
-        {"title":"Add To Cart", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D55425979%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"}        
+        {"title":"ADD TO CART", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D55425979%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"}        
     ].to_json
 
             when "<http://www.parents.com/fun/entertainment/books/best-childrens-books-of-the-year/>"
+			  text = "Here is what I found:"
               attachments = [
         {			
             "title": "Big Fish Little Fish",
 			      "title_link": "https://www.walmart.com/ip/Big-Fish-Little-Fish/49207133",
 			      "image_url":"http://images.parents.mdpcdn.com/sites/parents.com/files/styles/width_360/public/102787324.jpg"
         }, 
-        {"title":"Add To Cart", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D49207133%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"},
+        {"title":"ADD TO CART", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D49207133%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"},
         {			
             "title": "TouchThinkLearn: ABC",
 			      "title_link": "https://www.walmart.com/ip/ABC/51797530",
 			      "image_url":"https://www.walmart.com/ip/Graco-Contender-65-Convertible-Car-Seat-Choose-Your-Pattern/33396182"
         }, 
-        {"title":"Add To Cart", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D51797530%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"},
+        {"title":"ADD TO CART", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D51797530%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"},
         {			
             "title": "GOODNIGHT EVERYONE",
 			      "title_link": "https://www.walmart.com/ip/GOODNIGHT-EVERYONE/912129766",
 			      "image_url":"https://i5.walmartimages.com/asr/10b7cf0a-ebd6-4444-a030-8b93398ac4a2_1.5a608ea2e6bdacc29443ef5b278c2a53.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
         }, 
-        {"title":"Add To Cart", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D912129766%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"},
+        {"title":"ADD TO CART", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D912129766%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"},
         {			
             "title": "The Cookie Fiasco",
 			      "title_link": "https://www.walmart.com/ip/The-Cookie-Fiasco/52609219",
 			      "image_url":"https://i5.walmartimages.com/asr/c7a3da62-aefe-4176-a6eb-026f93d4005b_1.d9bdfc55775f3018e684afc58e330704.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
         }, 
-        {"title":"Add To Cart", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D52609219%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"}
+        {"title":"ADD TO CART", "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D52609219%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"}
     ].to_json
-            when "<https://www.makeupalley.com/>"
-              urls = "check what I've found\nhttps://www.walmart.com/ip/Orly-Bonder-Rubberized-Base-Coat-0-3-oz/227372525\nhttps://www.walmart.com/ip/wet-n-wild-Photo-Focus-Concealer-Wand-Light-Ivory/123328886\nhttps://www.walmart.com/ip/Estee-Lauder-Advanced-Night-Repair-Synchronized-Recovery-Complex-II-75ml-2-5oz/127132304\nhttps://www.walmart.com/ip/Estee-Lauder-Double-Wear-Stay-In-Place-Makeup-SPF-10-No-01-Fresco-2C3-30ml-1oz/152329402"
-            when "<https://ezinearticles.com/?cat=Home-Improvement:Furniture>"
-              urls = "check what I've found\nhttps://www.walmart.com/ip/Trespass-Marmor-Tufted-Nail-Head-Upholstered-Bed-Queen/43829183\nhttps://www.walmart.com/ip/Best-Choice-Products-Patio-Umbrella-Offset-10-Hanging-Umbrella-Outdoor-Market-Umbrella-New-Multiple-Colors/166312054\nhttps://www.walmart.com/ip/Solis-Ligna-Solid-Wood-Table-and-Bonded-Leather-Office-Chairs-11-piece-Conference-Set/54972176\nhttps://www.walmart.com/ip/Mainstays-Buchannan-Sofa-Dark-Chocolate-Microfiber/47218551"
-            when "<https://www.rottentomatoes.com/>"
-              urls = "check what I've found\nhttps://www.vudu.com/movies/#!content/850531/Annabelle-Creation\nhttps://www.vudu.com/movies/#!content/864891/Dunkirk-Season-1\nhttps://www.vudu.com/movies/#!content/884936/The-Hitmans-Bodyguard\nhttps://www.vudu.com/movies/#!content/851335/Spider-Man-Homecoming"
+            when "<https://ezinearticles.com/?cat=Home-Improvement:Furniture>>"
+			  text = "Here is what I found:"
+              attachments = [
+                {
+                 "title": "Right2Home Upholstered Low Profile Bed - Queen",
+                 "title_link": "https://www.walmart.com/ip/Right2Home-Upholstered-Low-Profile-Bed-Queen/39449500",
+                 "image_url": "https://i5.walmartimages.com/asr/adef3e1b-1d88-46c0-b52d-19b1b9930e5d_1.87c79c4d99a47b3c678ba909bbf8663d.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
+                },
+                {
+                 "title": "ADD TO CART",
+                 "title_link": "http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D39449500%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"
+                },
+                {
+                 "title": "Best Choice Products Patio Umbrella",
+                 "title_link": "https://www.walmart.com/ip/Best-Choice-Products-Patio-Umbrella-Offset-10-Hanging-Umbrella-Outdoor-Market-Umbrella-New-Multiple-Colors/166312054",
+                 "image_url": "https://i5.walmartimages.com/asr/f66ff1dc-6c57-4a58-884d-61dfa2ff1908_1.8020b606d6e937b11db20fe303c766e1.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
+                },
+                {
+                 "title": "ADD TO CART",
+                 "title_link": "http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D166312054%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"
+                },
+                {
+                 "title": "Mayline Napoli Curved End Conference Table",
+                 "title_link": "https://www.walmart.com/ip/Mayline-Napoli-Curved-End-Conference-Table-in-Golden-Cherry-12/41688481",
+                 "image_url": "https://i5.walmartimages.com/asr/b08e0412-9d9c-49bd-9c1e-ca185923e1fc_1.f5f94b5776ea0d8955cfe17ae8b24db4.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
+                },
+                {
+                 "title": "ADD TO CART",
+                 "title_link": "http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D41688481%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"
+                },
+                {
+                 "title": "Lifestyle Solutions Zola Sofa",
+                 "title_link": "https://www.walmart.com/ip/Lifestyle-Solutions-Zola-Sofa-Dark-Grey/55322805",
+                 "image_url": "https://i5.walmartimages.com/asr/ceda4c9d-19f2-4f35-989b-bd3a2e938834_1.6c1104c6ee25ee89ab88ee5894f53b0a.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
+                },
+                {
+                 "title": "ADD TO CART",
+                 "title_link": "http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D55322805%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"
+                }
+                ].to_json
+
+            when "<https://www.pinterest.com/pin/433823376584880457/>"
+			  text = "Here is what I found:"
+              attachments = [
+                {
+                 "title": "Bread flour",
+                 "title_link": "https://www.walmart.com/ip/Pilsbury-Bread-Flour-5-lbs/10308169",
+                 "image_url": "https://i5.walmartimages.com/asr/f5e5a6cf-56de-4bbc-98a2-e90e5a05d91d_1.7d974e54071cea00fd76ccc84675d44e.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
+                }, 
+                {
+                 "title":"ADD TO CART", 
+                 "title_link":"http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D10308169%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapihttp://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D10308169%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"
+                },
+                {
+                 "title": "Granulated sugar",
+                 "title_link": "https://www.walmart.com/ip/Domino-Premium-Sugar-Cane-Granulated-Sugar-4-lb-Bag/19500189",
+                 "image_url": "https://i5.walmartimages.com/asr/1349d0b4-bfcd-454c-8b9a-e9e3702e16c4_1.fec4ebcb6bf5d33d57118aba6c76cbad.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
+                },
+                {
+                 "title":"ADD TO CART",
+                 "title_link": "http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D19500189%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"
+                },
+                {
+                 "title": "Butter, unsalted",
+                 "title_link": "https://www.walmart.com/ip/Isigny-Unsalted-Butter-in-Basket/104722419",
+                 "image_url": "https://i5.walmartimages.com/asr/2856ac2e-df40-4c09-b6fc-8eea71d65add_1.4cb4757d79546a7c64b96695d217f294.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
+                },
+                {
+                 "title": "ADD TO CART",
+                 "title_link": "http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D104722419%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"
+                },
+                {
+                 "title": "Instant (quick-rise) yeast",
+                 "title_link": "https://www.walmart.com/ip/Red-Star-Quick-Rise-Instant-Yeast-3-25-oz-Packets/10319305",
+                 "image_url": "https://i5.walmartimages.com/asr/97f90f1f-0382-4c76-93e3-64dd25a88e1c_1.78a8db0558c09d43e9dc07ae625ad87c.jpeg?odnHeight=200&odnWidth=200&odnBg=FFFFFF"
+                },
+                {
+                 "title": "ADD TO CADT",
+                 "title_link": "http://c.affil.walmart.com/t/api02?l=http%3A%2F%2Faffil.walmart.com%2Fcart%2FaddToCart%3Fitems%3D10319305%7C1%26affp1%3Drba2WcSC_JIWqxXZi_v0-wKblIq8dKLZ3Y_MK_UN1qY%26affilsrc%3Dapi%26veh%3Daff%26wmlspartner%3Dreadonlyapi"
+                }
+                ].to_json
             else
-              urls = "I don't get it!!"
+              text = "Please enter a url or paste an image!!"
 		  end
 		end
 	  
       options = {
 	  token: @team.bot["bot_access_token"],
 	  channel: data["event"]["channel"],
-	  text: "Here is what I found:",
+	  text: text,
 	  as_user: false,
 	  attachments: attachments
 	  }
